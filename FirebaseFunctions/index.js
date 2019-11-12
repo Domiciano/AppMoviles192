c45981d843709687e25a73e26d6f84d9b9622b77
1143848922 <domiciano.rincon@correounivalle.edu.co>
2019-11-11 20:07:50 -0500

    Funciones de paginación

--- FirebaseFunctions/index.js
+++ FirebaseFunctions/index.js
@@ -1,35 +1,43 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express');
+const ITEMS = 5;
 
 admin.initializeApp();
 const app = express();
 app.use(express.json());
 
-app.get('/user', (req, res) => {
-  res.send("Received GET param: "+req.query.alfa);
+app.get('/param', (req, res) => {
+  res.send("Received GET param. Alfa: " + req.query.alfa + ", Beta: " + req.query.beta);
 });
 
 app.get('/photos', (req, res) => {
-
-	var out = [];
 	var fotos = [];
+	var out = [];
+	var page = req.query.page;
+	var album = req.query.uid;
 
-	admin.database().ref().child('fotos').child(req.query.uid).on('value', function(snap){
-		snap.forEach(
+	admin.database().ref()
+		.child('fotos')
+		.child(album)
+		.once('value', function(snap){
+
+			snap.forEach(
 				(item) => {
 					fotos.push( item.val() );														
 				}
 			);
 
-
-		for(var i = fotos.length-1 ; i>=0 ; i--) {
-			if( i >= parseInt(req.query.from) && i <= parseInt(req.query.to) ){
-				out.push(fotos[i]);
+			for(let i=page*ITEMS ; i < ITEMS + page*ITEMS - 1 ; i++){
+				if(fotos[i] !== undefined){
+					out.push(fotos[i]);
+				}
 			}
-		}
-		res.send(out);
+
+			res.send(out);
+
 	});
+	
 });
 
 exports.functions = functions.https.onRequest(app);
